86a1e7dbf6a39487f09ed9fe5f9f7db4934b4f01
alainmama <alainmama@gmail.com>
2022-05-23 18:14:46 -0500

    Insertar y eliminar datos de la base de datos

--- mysql/src/main/java/com/sic/App.java
+++ mysql/src/main/java/com/sic/App.java
@@ -18,6 +18,24 @@ public class App {
             while (rs.next())
                 //Impresión de los valores
                 System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3)+ " " + rs.getString(4));
+            
+            //Estos valores deben coincidir con los tipos de datos que se asignaron durante la creación de la tabla 
+            stmt.executeUpdate("INSERT INTO alumnos VALUES(2,'Santiago','López','20524700')");
+            rs = stmt.executeQuery("select * from alumnos");
+
+            System.out.println("Se agrego una nueva línea");
+            //Ciclo de todos los elementos obtenidos por el query
+            while (rs.next())
+                //Impresión de los valores
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3)+ " " + rs.getString(4));
+                stmt.executeUpdate("DELETE FROM alumnos WHERE id = 0;");
+                rs = stmt.executeQuery("select * from alumnos");
+    
+                System.out.println("Se elimino una nueva línea");
+                //Ciclo de todos los elementos obtenidos por el query
+                while (rs.next())
+                    //Impresión de los valores
+                    System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3)+ " " + rs.getString(4));
             //Se cierra la conexión 
             con.close();
         } catch (Exception e) {
